3a06448eb50c4b0819e2258c985a6bb3ae31fc29
jfar41 <68749301+jfar41@users.noreply.github.com>
2020-08-13 17:50:19 -0700

    Made the board workable

--- main.js
+++ main.js
@@ -7,45 +7,92 @@ const lookup = {
 
 
 /*---App's state variables---*/
-
+let board, turn, winner;
+board = [
+    [null,null,null,null,null,null, null],
+    [null,null,null,null,null,null, null],
+    [null,null,null,null,null,null, null],
+    [null,null,null,null,null,null, null],
+    [null,null,null,null,null,null, null],
+    [null,null,null,null,null,null, null]
+];
 
 /*---cached element references---*/
 let table = document.querySelector('container');
 let cells = document.querySelectorAll('td');
 let tableRow = document.getElementsByClassName('row');
 let showCurrentPlayer= document.getElementById('message');
+//eventually will do indexHTML to tell whose current player and/or who won
+
 let currentPlayer=1;
 /*---event listener---*/
 
 for(let i=0; i < cells.length;i++) {
     cells[i].addEventListener('click', (e) => {
         //check if working
-        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
+   
+        clickSlots(e.target.cellIndex);
     })
+    
 } 
+
+
+//to iterate through board
+//change clickSlots to where it's detecting column instead of row
+//clickSlots now works just fix above console.log to then assign to a variable
+//that can be passed into clickSlots
+
+//think of how to update i from down level and up. I thought of push but there might be a better way
+//if board[i] ==null  && currentPlayer==1 {board.pop}
+
+function clickSlots(colIdx) {
+    if (board[0][colIdx]) {
+        console.log('this role is filled');
+        return 
+    }
+    let isFilled = true;
+    let row = 5;
+    while (isFilled && row>=0) {
+        if (board[row][colIdx]== null) {
+            board[row][colIdx] = currentPlayer;
+            isFilled= false;
+            currentPlayer *= -1;
+            console.log(currentPlayer);
+            render();
+        } else {
+            row--;
+        }
+    }
+}
+
+
 
 
 
 /*---Functions---*/
 initialize();
-//
-
-
-
 
 
 //to reference for win logic
 function initialize() {
-    board = [
-        [null,null,null,null,null,null,null],
-        [null,null,null,null,null,null,null],
-        [null,null,null,null,null,null,null],
-        [null,null,null,null,null,null,null],
-        [null,null,null,null,null,null,null],
-        [null,null,null,null,null,null,null]
-    ];
+   
     
     turn = 1;
     winner = null;
     
 } 
+function render() {
+    //loop thru board and color cirlce depending on what's insde each element
+    for(let i=0; i<board.length; i++) {
+        for(let j=0; i< board.length;i++) {
+    if (board[i][j] ===1) {
+        cells.style.backgroundColor = 'red';
+    } else if (board[i][j] === -1) {
+        cells.style.backgroundColor = 'yellow'
+    } else {
+        cells.style.backgroundColor = 'white';
+    }
+
+    //if 1 make yellow (by this point already stated player 1 is yellow) n vice versa
+}
+}}
